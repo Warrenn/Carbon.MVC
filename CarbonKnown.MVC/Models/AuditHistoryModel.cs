8e85066fd303c1b4329e2d6ef7c3fb4545f673ab
warrenn@busyweb.co.za <warrenn@busyweb.co.za>
2014-03-27 10:06:45 +0200

    audith

---
+++ CarbonKnown.MVC/Models/AuditHistoryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CarbonKnown.MVC.Models
+{
+    public class AuditHistoryModel
+    {
+        public Guid? ActivityGroupId { get; set; }
+
+        public string CostCode { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
