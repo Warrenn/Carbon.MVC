0ddd5cd5b2f8a802f8424bcfae67b89c6a8ba460
warrenn@busyweb.co.za <warrenn@busyweb.co.za>
2014-07-30 19:35:18 +0200

    Auto Commit C:\Work Bench\development\CarbonKnown.MVC\TestDataSource\Controllers\HomeController.cs on 30-07-2014 07:31:49

---
+++ TestDataSource/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebMatrix.WebData;
+
+namespace TestDataSource.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+
+    }
+}
