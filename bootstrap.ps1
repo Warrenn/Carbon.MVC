40e2d8c1ccb53140b0d3c72d7c4cad2e99480eb9
Warrenn Enslin <warrenne@gmail.com>
2022-07-06 19:26:17 +0200

    sub path

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,12 +1,13 @@
 $SiteName = "Manage"
 $BasePath = "${env:MVC_BASE}"
+if([String]::IsNullOrWhiteSpace($BasePath)){$BasePath="C:\MVC\Inetpub\CarbonKnown"}
 Start-IISCommitDelay
 $Site=Get-IISSite $SiteName
 if ($Site -ne $null)  {
-    Remove-IISSite -Name $SiteName -Confirm
+    Remove-IISSite -Name $SiteName -Confirm:$false
 }
 $Site=New-IISSite -Name $SiteName -BindingInformation "129.232.194.210:80:manage.carbonknown.com" -PhysicalPath $BasePath -Passthru
 $Site.Applications["/"].ApplicationPoolName = ".NET v4.5"
 New-Item -ItemType Directory -Force -Path "$BasePath/Nampak"
-New-WebVirtualDirectory -Site $Site -Name "Ck3/Nampak" -PhysicalPath "$BasePath/Nampak"
+New-WebApplication -Name "Ck3/Nampak" -Site $SiteName -PhysicalPath "$BasePath\Nampak" -ApplicationPool ".NET v4.5"
 Stop-IISCommitDelay
