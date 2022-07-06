f110fe0802d2cba5425cde38cfdf4b1e7de2545c
Warrenn Enslin <warrenne@gmail.com>
2022-07-06 16:41:54 +0200

    adde y

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,5 +3,5 @@ $BasePath = "${env:MVC_BASE}"
 $Site=New-IISSite -Name "Manage" -BindingInformation "129.232.194.210:80:manage.carbonknown.com" -PhysicalPath $BasePath  -Passthru
 $Site.Applications["/"].ApplicationPoolName = ".NET v4.5"
 write-host $Site
-New-WebVirtualDirector -Site $Site -Name "Ck3/Nampak" -PhysicalPath "$BasePath/Nampak"
+New-WebVirtualDirectory -Site $Site -Name "Ck3/Nampak" -PhysicalPath "$BasePath/Nampak"
 Stop-IISCommitDelay
