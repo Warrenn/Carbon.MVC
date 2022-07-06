0e0bf0e9078c84503ed2a1c0671ca47d2d79b9ea
Warrenn Enslin <warrenne@gmail.com>
2022-07-06 16:11:02 +0200

    added pool

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,1 +1,4 @@
-
+Start-IISCommitDelay
+$Site=New-IISSite -Name "Manage" -BindingInformation "*:8080:" -PhysicalPath "C:\MVC\Inetpub\CarbonKnown" -Passthru
+$Site.Applications["/"].ApplicationPoolName = ".NET v4.5"
+Stop-IISCommitDelay
