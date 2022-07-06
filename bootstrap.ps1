3c49f40fb130b20db3d9210c9336820d21b34b14
Warrenn Enslin <warrenne@gmail.com>
2022-07-06 16:57:59 +0200

    confirm

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,7 +3,7 @@ $BasePath = "${env:MVC_BASE}"
 Start-IISCommitDelay
 $Site=Get-IISSite $SiteName
 if ($Site -ne $null)  {
-    Remove-IISSite -Name $SiteName -Force
+    Remove-IISSite -Name $SiteName -Confirm
 }
 $Site=New-IISSite -Name $SiteName -BindingInformation "129.232.194.210:80:manage.carbonknown.com" -PhysicalPath $BasePath -Passthru
 $Site.Applications["/"].ApplicationPoolName = ".NET v4.5"
