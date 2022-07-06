39c8a746a97a52d3ba80ee2f27e9c31490938148
Warrenn Enslin <warrenne@gmail.com>
2022-07-06 16:16:20 +0200

    changed ip

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,4 @@
 Start-IISCommitDelay
-$Site=New-IISSite -Name "Manage" -BindingInformation "*:8080:" -PhysicalPath "C:\MVC\Inetpub\CarbonKnown" -Passthru
+$Site=New-IISSite -Name "Manage" -BindingInformation "129.232.194.210:80:manage.carbonknown.com" -PhysicalPath "C:\MVC\Inetpub\CarbonKnown" -Passthru
 $Site.Applications["/"].ApplicationPoolName = ".NET v4.5"
 Stop-IISCommitDelay
