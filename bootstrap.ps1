2a350fd43c42b34ac9eeb2a0da8bbecba506a6e1
Warrenn Enslin <warrenne@gmail.com>
2022-07-06 16:52:00 +0200

    work

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,6 +1,11 @@
-Start-IISCommitDelay
+$SiteName = "Manage"
 $BasePath = "${env:MVC_BASE}"
-$Site=New-IISSite -Name "Manage" -BindingInformation "129.232.194.210:80:manage.carbonknown.com" -PhysicalPath $BasePath  -Passthru
+Start-IISCommitDelay
+$Site=Get-IISSite $SiteName
+if ($Site -ne $null)  {
+    Remove-IISSite $SiteName
+}
+$Site=New-IISSite -Name $SiteName -BindingInformation "129.232.194.210:80:manage.carbonknown.com" -PhysicalPath $BasePath -Passthru
 $Site.Applications["/"].ApplicationPoolName = ".NET v4.5"
 write-host $Site
 #New-WebVirtualDirectory -Site $Site -Name "Ck3/Nampak" -PhysicalPath "$BasePath/Nampak"
