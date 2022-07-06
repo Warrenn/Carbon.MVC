6cb2482cd81a1caf647a6e3f69bd4d2272057865
Warrenn Enslin <warrenne@gmail.com>
2022-07-06 16:40:51 +0200

    mvc base

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,5 +1,5 @@
 Start-IISCommitDelay
-$BasePath = "${env:MVC_BASE}\CarbonKnown"
+$BasePath = "${env:MVC_BASE}"
 $Site=New-IISSite -Name "Manage" -BindingInformation "129.232.194.210:80:manage.carbonknown.com" -PhysicalPath $BasePath  -Passthru
 $Site.Applications["/"].ApplicationPoolName = ".NET v4.5"
 write-host $Site
