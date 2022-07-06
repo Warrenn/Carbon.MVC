e2b1f2e9323e3eddde4e56adf0e6c9ce30ce9df4
Warrenn Enslin <warrenne@gmail.com>
2022-07-06 16:53:47 +0200

    added remove

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,10 +3,10 @@ $BasePath = "${env:MVC_BASE}"
 Start-IISCommitDelay
 $Site=Get-IISSite $SiteName
 if ($Site -ne $null)  {
-    Remove-IISSite $SiteName
+    Remove-IISSite $SiteName -Confirm $true
 }
 $Site=New-IISSite -Name $SiteName -BindingInformation "129.232.194.210:80:manage.carbonknown.com" -PhysicalPath $BasePath -Passthru
 $Site.Applications["/"].ApplicationPoolName = ".NET v4.5"
 write-host $Site
-#New-WebVirtualDirectory -Site $Site -Name "Ck3/Nampak" -PhysicalPath "$BasePath/Nampak"
+New-WebVirtualDirectory -Site $Site -Name "Ck3/Nampak" -PhysicalPath "$BasePath/Nampak"
 Stop-IISCommitDelay
