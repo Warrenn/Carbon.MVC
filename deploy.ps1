c8b702f8c0ab16f807339cf1b80563c99374e810
Sever <you@example.com>
2022-07-06 20:30:50 +0200

    updated deploy

--- deploy.ps1
+++ deploy.ps1
@@ -10,6 +10,8 @@ $RepoUrl = "https://github.com/Warrenn/Carbon.MVC/releases/download/$Release/$Ar
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 Set-ExecutionPolicy Bypass -Scope Process -Force; 
 $ParentPath= Resolve-Path ".." 
-iex ((New-Object System.Net.WebClient).DownloadFile($RepoUrl, "$ParentPath/$Artifact")) 
+New-Item -ItemType Directory -Force -Path "$ParentPath/$Client"
+Write-Host "$RepoUrl"
+(New-Object System.Net.WebClient).DownloadFile($RepoUrl, "$ParentPath/$Client/$Artifact")
 
-
+Expand-Archive -Path "$ParentPath/$Client/$Artifact" -DestinationPath "$ParentPath\Inetpub\CarbonKnown\$Client"
