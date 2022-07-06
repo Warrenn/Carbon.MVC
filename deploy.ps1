8e51f5b262aca1732a577bcc0a5c711f0e642202
Warrenn Enslin <warrenne@gmail.com>
2022-07-06 19:59:53 +0200

    added deploy

---
+++ deploy.ps1
@@ -0,0 +1,14 @@
+ Param(
+    $Artifact = "CarbonKnown.MVC.zip",
+    $Release = "v1.0.0",
+    [Parameter(Mandatory=$true)]
+    $Client
+)
+
+$RepoUrl = "https://github.com/Warrenn/Carbon.MVC/releases/download/$Release/$Artifact"
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+Set-ExecutionPolicy Bypass -Scope Process -Force; 
+iex ((New-Object System.Net.WebClient).DownloadFile($RepoUrl, $Artifact)) 
+
+
